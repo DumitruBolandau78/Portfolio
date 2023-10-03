33e041cc021f637e1a6ecd005e896b110be9ed90
Dima <dumitruwebdev@gmail.com>
2023-10-04 01:57:35 +0300

    master

--- js/downloadCV.js
+++ js/downloadCV.js
@@ -2,7 +2,7 @@ const downloadCvBtn = document.querySelector(".download-cv button");
 
 downloadCvBtn.addEventListener("click", async () => {
   try {
-    const response = await fetch("/data/cv.pdf");
+    const response = await fetch("/data/CV.pdf");
     const file = await response.blob();
     const link = document.createElement("a");
     link.href = URL.createObjectURL(file);
